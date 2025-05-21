8f9befdc0470c587115cade1765b263c37a8487b
Manoj-V07 <manoj.v2024cse@sece.ac.in>
2025-05-21 19:08:33 +0530

    Search part

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,5 +1,25 @@
-export const Search = () => {
+import { Card } from "../components/Card";
+import { useFetch } from "../hooks/useFetch";
+import { useSearchParams } from "react-router-dom";
+
+export const Search = ({apiPath}) => {
+  
+  const [searchParams] = useSearchParams();
+  const queryTerm = searchParams.get("q");
+  const { data : movies } = useFetch(apiPath , queryTerm);
+   
   return (
-    <main>Search</main>
+    <main>
+      <section className="py-7">
+        <p className="text-3xl text-gray-700 dark:text-white">{ movies.length === 0 ? `No result found for '${queryTerm}'` : `Result for '${queryTerm}'`}</p>
+      </section>
+      <section className="max-w-7xl mx-auto py-7">
+        <div className="flex justify-start flex-wrap gap-5">
+          {movies.map((movie) => (
+            <Card key={movie.id} movie={movie}/>
+          )) }
+        </div>
+      </section>
+    </main>
   )
 }
