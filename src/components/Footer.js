5e7d60e54ad8697c972e1b1e6fc1b5f1bea0d873
Manoj-V07 <manoj.v2024cse@sece.ac.in>
2025-05-23 19:15:34 +0530

    Footer Update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,16 +7,16 @@ export const Footer = () => {
           <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2030 <Link to="/" className="hover:underline">Cinemate™</Link>. All Rights Reserved.</span>
           <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
           <li>
-            <a href="#" target="_blank" className="hover:underline me-4 md:me-6">Instagram</a>
+            <a href="https://www.instagram.com/manoj_v7689/?next=%2F" target="_blank" className="hover:underline me-4 md:me-6">Instagram</a>
           </li>
           <li>
-            <a href="#" target="_blank" className="hover:underline me-4 md:me-6">LinkedIn</a>
+            <a href="https://www.linkedin.com/in/manoj-v-646218333/" target="_blank" className="hover:underline me-4 md:me-6">LinkedIn</a>
           </li>
           <li>
-            <a href="#" target="_blank" className="hover:underline me-4 md:me-6">Youtube</a>
+            <a href="" target="_blank" className="hover:underline me-4 md:me-6">Youtube</a>
           </li>
           <li>
-            <a href="#" target="_blank" className="hover:underline">Github</a>
+            <a href="" target="_blank" className="hover:underline">Github</a>
           </li>
           </ul>
         </div>
